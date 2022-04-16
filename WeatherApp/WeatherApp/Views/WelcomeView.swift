4fbbcacf4ee6ef07678c6a25c176f122dc23f0bb
Antonella L <48662612+aml2507@users.noreply.github.com>
2022-04-16 00:53:47 -0300

    Base app

---
+++ WeatherApp/WeatherApp/Views/WelcomeView.swift
@@ -0,0 +1,43 @@
+//
+//  WelcomeView.swift
+//  WeatherApp
+//
+//  Created by Antonella Mariel Lesta on 15/04/2022.
+//
+
+import SwiftUI
+import CoreLocationUI
+
+struct WelcomeView: View {
+    @EnvironmentObject var locationManager: LocationManager
+
+    var body: some View {
+        VStack{
+            VStack(spacing: 20){
+                Text("Welcome to the Weather App")
+                    .bold().font(.title)
+                Text("Please share your current location to get the weather on your area")
+                    .padding()
+            }
+            .multilineTextAlignment(.center)
+            .padding()
+            
+            LocationButton(.shareCurrentLocation){
+                locationManager.requestLocation()
+            }
+            .cornerRadius(30)
+            .symbolVariant(.fill)
+            .foregroundColor(.white)
+            
+            
+        }
+        .frame(maxWidth: .infinity,
+               maxHeight: .infinity)
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
